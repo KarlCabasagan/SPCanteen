357de1b563a7f4b2363b321f47c7d8d76b3a0767
Karl Cabasagan <karl.cabasagan.202200008@gmail.com>
2024-03-17 05:19:42 +0800

    Installed more commands for Laravel & Added forgotPassword page

---
+++ resources/views/forgotPassword.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" href="{{asset('/fontawesome-free-6.5.1-web/css/all.min.css')}}">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+
+    <title>Forgot Password</title>
+</head>
+<body>
+    
+</body>
+</html>
