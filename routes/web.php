41c6399bdc20c878fb7f684af6699a898a1b2c21
Karl Cabasagan <karl.cabasagan.202200008@gmail.com>
2024-03-17 12:43:21 +0800

    Added forgot password page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/forgotpassword', function () {
+    return view('forgotPassword');
+});
