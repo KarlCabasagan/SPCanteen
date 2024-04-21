67e3789ff164533b0d2adf738baf786cc102507b
Nostalgic8803 <romarcbongcaron.2022.01308@gmail.com>
2024-04-21 14:49:00 +0800

    added design in auth

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="@yield('css', 'css/main.css')">
     <title>@yield('title', 'SPCanteen')</title>
 </head>
 <body>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,18 +5,45 @@
         <p>Set up profile</p>
         <a href="/">Home</a>
     @else
+        @section('css', 'css/auth.css')
         @section('title', 'SPCanteen - Register')
-        <div class="registrationForm">
-            <form action="/register" method="POST">
-                @csrf
-                <input type="text" id="name" name="name" placeholder="Username">
-                <input type="text" id="email" name="email" placeholder="example@example.com">
-                <input type="password" id="password" name="password" placeholder="Password">
-                <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirm Password">
-                <input type="submit" value="Register">
-            </form>
-            <p>Already have an account? <a href="/">Login</a></p>
+        <div class="container">
+            <div class="registration-form">
+                <div class="logo2">
+                    <img id="logo" src="/images/SPCanteen.png" alt="SPCanteen.png">
+                </div>
+                <div class="form">
+                    <form action="/register" method="POST">
+                        @csrf
+                        <div class="input-container">
+                            <input type="text" name="username" class="input-field" required>
+                            <label>Username</label>
+                            <i class="fa-solid fa-user"></i>
+                        </div>
+                        <div class="input-container">
+                            <input type="text" name="email" class="input-field" required>
+                            <label>Email</label>
+                            <i class="fa-solid fa-envelope"></i>
+                        </div>
+                        <div class="input-container">
+                            <input type="password" name="password" class="input-field" required>
+                            <label>Password</label>
+                            <i class="fa-solid fa-eye"></i>
+                        </div>
+                        <div class="input-container">
+                            <input type="password" name="confirm-password" class="input-field" required>
+                            <label>Confirm Password</label>
+                            <i class="fa-solid fa-eye"></i>
+                        </div>
+                        <div class="register">
+                            <input type="submit" class="register-btn" value="REGISTER">
+                        </div>
+                    </form>
+                    <div class="login">
+                        <p id="login-txt">Already have an account? <a id="login-btn" href="/">Login</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
     @endauth
-    
 @endsection
